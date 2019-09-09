fbb92d8ff2f43c1147444d75ea6a41f15a92105e
Shadi Ghelman <Shadi.Ghelman72@gmail.com>
2019-09-09 21:01:42 +0430

    AddButton

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-
+	@IBAction func buttonDidTouch(_ sender: Any) {
+		print("Hi")
+	}
+	
 }
 
